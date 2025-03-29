abf976a5ee817037a32d9124c5499cfbe7b84519
Abhinav Peri <abhinavperi16@gmail.com>
2025-03-29 16:40:37 -0500

    adding print

--- isaacgymenvs/tasks/allegro_kuka/allegro_kuka_juggle_base.py
+++ isaacgymenvs/tasks/allegro_kuka/allegro_kuka_juggle_base.py
@@ -1024,6 +1024,11 @@ class AllegroKukaJuggleBase(VecTask):
         has_caught_reward = ((self.has_caught == 1) & (self.prev_has_caught == 0)).sum(dim=1).float()
 
         catch_height_reward = (((self.has_caught == 1) & (self.prev_has_caught == 0)).float() * torch.abs(self.catch_deltas).min(dim=-1)[0]).sum(dim=-1)
+        print("-" * 40)
+        print(f"Throw Indicators: Prev {self.prev_has_thrown[0, 0].item()}, Cur {self.has_thrown[0, 0].item()}")
+        print(f"Caught Indicators: Prev {self.prev_has_caught[0, 0].item()}, Cur {self.has_caught[0, 0].item()}")
+        print(f"Catch Deltas: Positive Delta {self.catch_deltas[0, 0, 0].item()}, Negative Delta {self.catch_deltas[0, 0, 1].item()}")
+        print(f"Rewards: Throw {has_thrown_reward[0]}, Caught {has_caught_reward[0]}, Catch Height {catch_height_reward[0]}")
         # downward toss reward
         # juggle_reward = ((self.juggle_state == 0) & (self.prev_juggle_state == 1) & (self.object_linvel[:, :, 2] <= 0)).sum(dim=1).float() 
 
